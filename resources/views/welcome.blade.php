4db031a61f8ad9e5c5ff620e5546c3606442bd5e
Alonso Chavarria <alonso@avotz.com>
2021-07-07 13:50:35 -0600

    se agrego ssss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
     </head>
     <body class="antialiased">
         <div id="app">
-            <h2>Vue Example</h2>
+            <h2>Vue Examplesssssssssssssssssss</h2>
             <example-component></example-component>
             <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
                 @if (Route::has('login'))
